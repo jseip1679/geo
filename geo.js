e7e66a449903e2a674a9c81dda6d6714629c4e29
Jake Seip <jake.seip@gmail.com>
2013-10-27 18:45:08 -0700

    fix haversine formula.  Mapping algorithm complete

--- geo.js
+++ geo.js
@@ -2,34 +2,54 @@
 
 var csv = require('csv');
 var fs = require('fs');
+var _ = require ('underscore');
 
 
-var comparisonCoordinates = {"San Francisco" : [37.7756, -122.4193]};//WIP
+var citiesToMapTo = [];
+var mappings = [];
 
+
+//Calculate the distance between two coordinates (http://www.movable-type.co.uk/scripts/latlong.html)
 var calcDistance = function(lat1,lon1,lat2,lon2){
-  //haversine formula borrowed from http://www.movable-type.co.uk/scripts/latlong.html
-
-  var R = 6371; // km
-  var dLat = (lat2-lat1)/(Math.PI/180);
-  var dLon = (lon2-lon1)/(Math.PI/180);
-  lat1 = lat1/(Math.PI/180);
-  lat2 = lat2/(Math.PI/180);
+  var R = 3949.99; // pulled from http://en.wikipedia.org/wiki/Earth_physical_characteristics_tables
+  var dLat = (lat2-lat1)*(Math.PI/180);
+  var dLon = (lon2-lon1)*(Math.PI/180);
+  lat1 = lat1*(Math.PI/180);
+  lat2 = lat2*(Math.PI/180);
 
   var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
           Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c;
-  return d*0.621371;
+  return R * c;
+};
+
+//Iterate through all cities, map them to the closest city that's already been mapped
+var mapAllCities = function(){
+  var distanceThreshold = 50; //miles
+
+  _.each(citiesToMapTo, function(city,index){
+    var closestCity = city.name;
+    var closestCityDistance = 1000;
+    _.each(mappings, function(mapTo){
+      if(mapTo.name === mapTo.closestCity){
+        var distance = calcDistance(city.coords[0], city.coords[1], mapTo.coords[0],mapTo.coords[1]);
+
+        if(distance < distanceThreshold && distance < closestCityDistance ){
+          closestCity = mapTo.name;
+          closestCityDistance = distance;
+        }
+      }
+    });
+    mappings.push({name:city.name, coords:city.coords, closestCity:closestCity});
+  });
+  console.log(mappings);
+  return mappings;
 };
 
 //Real CSV parsing done here
-csv().from.stream(fs.createReadStream('/Users/jakeseip/Desktop/zip_codes_states.csv'))//__dirname+
-.to.path(__dirname+'/loadsWithDistance.csv').transform(function(row){
-  var dist = calcDistance(row[1],row[2], 37.765704,-122.442069); //WIP
-  row.push(dist);
-  return row;
-}).on('record', function(row){
-  console.log(row); //let us see what we parsed through
+csv().from.stream(fs.createReadStream('/Users/jakeseip/Desktop/locations.csv')).on('record', function(row){
+  citiesToMapTo.push({coords:[row[2]*1,row[3]*1], name: row[1]});
 }).on('end', function(count){
-  console.log('Number of lines: '+count);
+  console.log('Parsed ', count, 'lines');
+  mapAllCities();
 }).on('error', function(err){console.log("ERROR:", err);});
